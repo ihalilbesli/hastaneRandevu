00f68d5e28d29fcba033ad07671801f47fd88c96
Your Name <your-email@example.com>
2025-03-10 17:49:19 +0300

    Yeni değişiklikler eklendi

--- hastarandevu/src/main/java/com/hastanerandevu/app/model/Appointments.java
+++ hastarandevu/src/main/java/com/hastanerandevu/app/model/Appointments.java
@@ -3,6 +3,9 @@ package com.hastanerandevu.app.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Entity
 @Table(name = "appointments")
 @Getter
@@ -15,5 +18,39 @@ public class Appointments {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User patient;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private User doctor;
+
+    @Column
+    private LocalDate date;
+
+    @Column
+    private LocalTime minute;
+
+    @Column(columnDefinition = "Text")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status=Status.AKTIF;
+
+
+
+    public enum Status{
+    AKTIF,IPTAL_EDILDI
+    }
+
+    @PrePersist
+    @PreUpdate
+    protected void formatTime(){
+        if(this.minute !=null){
+            this.minute=this.minute.withSecond(0).withNano(0);
+        }
+    }
 
 }
