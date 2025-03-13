ebc78fae5071e63be55ead7bb1a3c4fd6620f64a
ihalilbesli <ihalilbesli@gmail.com>
2025-03-13 18:25:31 +0300

    Veritabani (Entitiyler) Olusturuldu

---
+++ hastarandevu/src/main/java/com/hastanerandevu/app/model/PatientReports.java
@@ -0,0 +1,33 @@
+package com.hastanerandevu.app.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Patienr_reports")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PatientReports {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private User doctor; // Raporu ekleyen doktor
+
+    @Column(nullable = false)
+    private String reportType;  // Ornek: "MR Sonucu"
+
+    @Column(nullable = false)
+    private String fileUrl;     //Yuklenen Rapor yolu veya resim yolu
+
+    @Column(nullable = false)
+    private LocalDate reportDate;
+
+}
---
+++ hastarandevu/src/main/java/com/hastanerandevu/app/model/TestResult.java
@@ -0,0 +1,41 @@
+package com.hastanerandevu.app.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Test_result")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TestResult {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id",nullable = false)
+    private User patient;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id",nullable = false)
+    private User doctor;    // Sonucu ekleyen doktor
+
+    @Column(nullable = false)   // Ornek: "Kan Tahlili"
+    private String testName;
+
+    @Column(nullable = false,columnDefinition = "TEXT")
+    private String result;
+
+    @Column(columnDefinition = "TEXT")
+    private String doctorComment;   // Doktor yorumu (Opsiyonel)
+
+    @Column(nullable = false)
+    private LocalDate testDate;
+
+
+}
