42cd1bc1639dcd43c007bde41ff5a6b2605b249d
ihalilbesli <ihalilbesli@gmail.com>
2025-04-24 17:59:24 +0300

    test

--- hastarandevu/src/main/java/com/hastanerandevu/app/model/Appointments.java
+++ hastarandevu/src/main/java/com/hastanerandevu/app/model/Appointments.java
@@ -14,7 +14,7 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @Builder
 public class Appointments {
-
+//
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  long id;
